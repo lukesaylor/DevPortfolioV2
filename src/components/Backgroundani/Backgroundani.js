c302daa84f3dd10bb9a42799cd66ae54ba582093
Lukas Saylor <slappyhoursound@gmail.com>
2020-05-16 19:05:50 -0400

    animation css

--- src/components/Backgroundani/Backgroundani.js
+++ src/components/Backgroundani/Backgroundani.js
@@ -2,27 +2,142 @@ import React from 'react';
 import './Backgroundani.css';
 import Squareani from './squareani';
 
-const Backgrounani = () => (
+const Backgroundani = () => (
     <div className="Backgroundani">
-    <Squareani/>
-    <Squareani 
-       h="500px"
-       w="500px"
-       toppad="50%"
-       z="-15"
-       hex="#6FFFE9"
-       time="25s"
-        />
-    <Squareani 
-        h="375px"
-        w="375px"
-        toppad="35%"
-        z="10"
-        hex="#0B132B"
-        time="5s"
-        />
+        <Squareani 
+            height="400px"
+            width="200px"
+            top="0%"
+            z="13"
+            hex="#6FFFE9"
+            ani="squareslide 40s infinite"
+            />
+        <Squareani 
+            height="200px"
+            width="200px"
+            top="30%"
+            z="10"
+            hex="#0B132B"
+            ani="squareslide 80s infinite"
+            />
+        <Squareani 
+            height="500px"
+            width="200px"
+            top="25%"
+            z="9"
+            hex="#6FFFE9"
+            ani="squareslide 60s infinite"
+            />
+        <Squareani 
+            height="250px"
+            width="300px"
+            top="75%"
+            z="9"
+            hex="#5BC0BE"
+            ani="squareslide 20s infinite"
+            />
+        <Squareani 
+            height="400px"
+            width="200px"
+            top="10%"
+            z="13"
+            hex="#6FFFE9"
+            ani="squaresliderev 40s infinite"
+            />
+        <Squareani 
+            height="200px"
+            width="200px"
+            top="40%"
+            z="10"
+            hex="#0B132B"
+            ani="squaresliderev 80s infinite"
+            />
+        <Squareani 
+            height="500px"
+            width="200px"
+            top="35%"
+            z="9"
+            hex="#6FFFE9"
+            ani="squaresliderev 60s infinite"
+                />
+        <Squareani 
+            height="250px"
+            width="300px"
+            top="85%"
+            z="9"
+            hex="#5BC0BE"
+            ani="squaresliderev 20s infinite"
+            />
+        
+
+        <Squareani 
+            height="40px"
+            width="40px"
+            top="5%"
+            z="13"
+            hex="#ffffff"
+            ani="squareslide 10s infinite"
+            />
+        <Squareani 
+            height="40px"
+            width="40px"
+            top="25%"
+            z="10"
+            hex="#ffffff"
+            ani="squareslide 8s infinite"
+            />
+        <Squareani 
+            height="40px"
+            width="40px"
+            top="30%"
+            z="9"
+            hex="#ffffff"
+            ani="squareslide 4s infinite"
+            />
+        <Squareani 
+            height="40px"
+            width="40px"
+            top="78%"
+            z="9"
+            hex="#ffffff"
+            ani="squareslide 9s infinite"
+            />
+        <Squareani 
+            height="40px"
+            width="40px"
+            top="15%"
+            z="13"
+            hex="#ffffff"
+            ani="squaresliderev 6s infinite"
+            />
+        <Squareani 
+            height="40px"
+            width="40px"
+            top="45%"
+            z="10"
+            hex="#ffffff"
+            ani="squaresliderev 12s infinite"
+            />
+        <Squareani 
+            height="40px"
+            width="40px"
+            top="40%"
+            z="9"
+            hex="#ffffff"
+            ani="squaresliderev 8s infinite"
+            />
+        <Squareani 
+            height="40px"
+            width="40px"
+            top="90%"
+            z="9"
+            hex="#ffffff"
+            ani="squaresliderev 7s infinite"
+                />
+        
 
     </div>
-)
+     )
+   
 
-export default Backgrounani;
+export default Backgroundani;
--- src/components/Backgroundani/squareani.js
+++ src/components/Backgroundani/squareani.js
@@ -5,27 +5,17 @@ import './squareani.css';
 const Squareani = (props) => (
     <div className="square"
         style={{
+            boxSizing:"border-box",
+            boxShadow:"20px 20px 10px",
             position:"fixed",
-            height:"{props.h}",
-            width:"{props.w}",
-            paddingTop:"{props.toppad}",
-            zIndex:"{props.z}",
-            backgroundColor:"{props.hex}",
-            opacity:".4",
+            height: props.height,
+            width: props.width,
+            top: props.top,
+            zIndex: props.z,
+            backgroundColor:props.hex,
+            opacity:".1",
             borderRadius:"20px",
-            animation:"squareslide {props.time} infinite"
-        /*style={{
-            position:"fixed",
-            height:"300px",
-            width:"300px",
-            paddingTop:"10%",
-            zIndex:"5",
-            backgroundColor:"green",
-            opacity:".4",
-            borderRadius:"20px",
-            animation:"squareslide 8s infinite"*/
-
-
+            animation: props.ani
             }}
     
     ></div>
