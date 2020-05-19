524add14f5d2fa55cf821f4a90f83e39fd2c241b
Lukas Saylor <slappyhoursound@gmail.com>
2020-05-18 20:47:46 -0400

    skills/about page

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import './Skills.css';
-
+import SkillsIconContainer from './SkillsIconContainer';
+import {ReactComponent as Githublogo} from '../../Assets/github.svg';
+import {ReactComponent as PhotoshopLogo} from '../../Assets/SkillsIcons/adobephotoshop.svg';
+import {ReactComponent as BootstrapLogo} from '../../Assets/SkillsIcons/bootstrap.svg';
+import {ReactComponent as CSS3Logo} from '../../Assets/SkillsIcons/css3.svg';
+import {ReactComponent as HTML5Logo} from '../../Assets/SkillsIcons/html5.svg';
+import {ReactComponent as JavascriptLogo} from '../../Assets/SkillsIcons/javascript.svg';
+import {ReactComponent as NPMLogo} from '../../Assets/SkillsIcons/npm.svg';
+import {ReactComponent as ReactLogo} from '../../Assets/SkillsIcons/react.svg';
+import {ReactComponent as ReactRouterLogo} from '../../Assets/SkillsIcons/reactrouter.svg';
+import {ReactComponent as SassLogo} from '../../Assets/SkillsIcons/sass.svg';
+import {ReactComponent as VisualStudioLogo} from '../../Assets/SkillsIcons/visualstudiocode.svg';
 
 const Skills = () => (
     <div className="Skillscontainer">
-    <h1 className="Skillsheader">Skills</h1>
-
+            <h1 className="Skillsheader">Skills</h1>
+        <div className="SkillsContent">
+            <div className="SkillsContentRow">
+                <div className="SkillsContentColumn"> 
+                    <SkillsIconContainer><JavascriptLogo/> </SkillsIconContainer>
+                    <SkillsIconContainer><HTML5Logo/> </SkillsIconContainer>
+                    <SkillsIconContainer><CSS3Logo/> </SkillsIconContainer>
+                </div>
+                <div className="SkillsContentColumn"> 
+                    <SkillsIconContainer><ReactLogo/> </SkillsIconContainer>
+                    <SkillsIconContainer><BootstrapLogo/> </SkillsIconContainer>
+                    <SkillsIconContainer><Githublogo/> </SkillsIconContainer>
+                </div>
+                <div className="SkillsContentColumn"> 
+                    <SkillsIconContainer><NPMLogo/> </SkillsIconContainer>
+                    <SkillsIconContainer><VisualStudioLogo/> </SkillsIconContainer>
+                    <SkillsIconContainer><ReactRouterLogo/> </SkillsIconContainer>
+                </div>
+                <div className="SkillsContentColumn"> 
+                    <SkillsIconContainer><SassLogo/> </SkillsIconContainer>
+                    <SkillsIconContainer><PhotoshopLogo/> </SkillsIconContainer>
+                </div>
+            </div>
+        </div>
     </div>
 )
 
---
+++ src/components/Skills/SkillsIconContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SkillsIconContainer.css';
+
+const SkillsIconContainer = (props) => (
+    <div className="SkillsIconContainer">
+         
+        {props.children}
+    </div>
+
+);
+
+export default SkillsIconContainer;
