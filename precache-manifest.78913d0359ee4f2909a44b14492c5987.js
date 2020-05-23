cdca094669c6e1c67a7fed2f3e757faab094e507
Lukas Saylor <slappyhoursound@gmail.com>
2020-05-23 15:31:05 -0400

    Updates

---
+++ precache-manifest.78913d0359ee4f2909a44b14492c5987.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77f4f16baec34028a967107df313d3ca",
+    "url": "/DevPortfolioV2/index.html"
+  },
+  {
+    "revision": "1a5942020cc679107677",
+    "url": "/DevPortfolioV2/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "430778270ed4ac93521f",
+    "url": "/DevPortfolioV2/static/css/main.30067ebe.chunk.css"
+  },
+  {
+    "revision": "1a5942020cc679107677",
+    "url": "/DevPortfolioV2/static/js/2.516d2b9d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/DevPortfolioV2/static/js/2.516d2b9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "430778270ed4ac93521f",
+    "url": "/DevPortfolioV2/static/js/main.9a6f89e6.chunk.js"
+  },
+  {
+    "revision": "cffda4d198f1b8184ae6",
+    "url": "/DevPortfolioV2/static/js/runtime-main.9ddf1694.js"
+  },
+  {
+    "revision": "9fe23bf225eecfcdb736e063b08a6123",
+    "url": "/DevPortfolioV2/static/media/Arimo-Regular.9fe23bf2.ttf"
+  },
+  {
+    "revision": "259d4afad7edca07e727ef80f5bbce07",
+    "url": "/DevPortfolioV2/static/media/Bungee-Regular.259d4afa.ttf"
+  },
+  {
+    "revision": "ef898acbf8bee9cd543d71dfbeebfb6a",
+    "url": "/DevPortfolioV2/static/media/BungeeShade-Regular.ef898acb.ttf"
+  },
+  {
+    "revision": "8068efbc82d412a6c52732759947bd12",
+    "url": "/DevPortfolioV2/static/media/FacialApp.8068efbc.png"
+  },
+  {
+    "revision": "8cb7dbcc7d2fcabc44fc29cb76c9a19a",
+    "url": "/DevPortfolioV2/static/media/Headshot.8cb7dbcc.png"
+  },
+  {
+    "revision": "5e9ac527b28637915bcbed15ab08caf9",
+    "url": "/DevPortfolioV2/static/media/Homemain.5e9ac527.png"
+  },
+  {
+    "revision": "9fe53565a7ea5a25b11070ad0a57c71d",
+    "url": "/DevPortfolioV2/static/media/Homemobile.9fe53565.png"
+  },
+  {
+    "revision": "9b0ef868591071d150934b55304aaef3",
+    "url": "/DevPortfolioV2/static/media/LSicon.9b0ef868.png"
+  },
+  {
+    "revision": "9b94499ccea3bd82b24cb210733c4b5e",
+    "url": "/DevPortfolioV2/static/media/Pacifico-Regular.9b94499c.ttf"
+  },
+  {
+    "revision": "3c4ae56cde20472a8937d65de935d1f1",
+    "url": "/DevPortfolioV2/static/media/PigDice.3c4ae56c.png"
+  },
+  {
+    "revision": "835b08108b0ac7619bb8bd9e14da9952",
+    "url": "/DevPortfolioV2/static/media/RoboPals.835b0810.png"
+  },
+  {
+    "revision": "2a8741d7b025bfb96ee3e484eb12dab0",
+    "url": "/DevPortfolioV2/static/media/TheBills.2a8741d7.png"
+  },
+  {
+    "revision": "bc6a3137fd2a012c5fd980c5ddab405a",
+    "url": "/DevPortfolioV2/static/media/adobephotoshop.bc6a3137.svg"
+  },
+  {
+    "revision": "b1b360f517849f9eb939165d18738714",
+    "url": "/DevPortfolioV2/static/media/bootstrap.b1b360f5.svg"
+  },
+  {
+    "revision": "b675af6016eed403de51899be75efe59",
+    "url": "/DevPortfolioV2/static/media/css3.b675af60.svg"
+  },
+  {
+    "revision": "617870e422d4b75087cdcf9ff298032e",
+    "url": "/DevPortfolioV2/static/media/github.617870e4.svg"
+  },
+  {
+    "revision": "3056bf2f96c40cd0a814b355959a6983",
+    "url": "/DevPortfolioV2/static/media/html5.3056bf2f.svg"
+  },
+  {
+    "revision": "1f6069572c9380a733700436d2980f8a",
+    "url": "/DevPortfolioV2/static/media/javascript.1f606957.svg"
+  },
+  {
+    "revision": "edbde22caf5910d6000b32efc65c42f3",
+    "url": "/DevPortfolioV2/static/media/leetcode.edbde22c.svg"
+  },
+  {
+    "revision": "619231ff8ec7a6b4d39aede30fa9890e",
+    "url": "/DevPortfolioV2/static/media/linkedin.619231ff.svg"
+  },
+  {
+    "revision": "e1d7fcc979c1d40ac116c43a21cb97df",
+    "url": "/DevPortfolioV2/static/media/node-dot-js.e1d7fcc9.svg"
+  },
+  {
+    "revision": "06851a1932d65340db2c1ba0faf7d6ce",
+    "url": "/DevPortfolioV2/static/media/npm.06851a19.svg"
+  },
+  {
+    "revision": "092e0882358cd09dbe9376b64d0f2a0e",
+    "url": "/DevPortfolioV2/static/media/react.092e0882.svg"
+  },
+  {
+    "revision": "264b63ab6c93861decbc80680776c266",
+    "url": "/DevPortfolioV2/static/media/reactrouter.264b63ab.svg"
+  },
+  {
+    "revision": "643d0db4b0852bdf249ac130d63d1fd6",
+    "url": "/DevPortfolioV2/static/media/sass.643d0db4.svg"
+  },
+  {
+    "revision": "112902ea6d337b292ee57c75b5391479",
+    "url": "/DevPortfolioV2/static/media/securityplus.112902ea.png"
+  },
+  {
+    "revision": "4fb1422fc808a2687aa77a9f72a4cf6f",
+    "url": "/DevPortfolioV2/static/media/visualstudiocode.4fb1422f.svg"
+  }
+]);
