9d6539b67d1793f30ccceb69a590db4ab38ff3ad
Lukas Saylor <slappyhoursound@gmail.com>
2020-05-24 15:31:21 -0400

    Project added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Projects.css';
 import Carousel  from 'react-bootstrap/Carousel';
- 
+import DevPortfolio from '../../Assets/ProjectImages/DevPortfolio.png'; 
 import FacialApp from '../../Assets/ProjectImages/FacialApp.png';
 import PigDice from '../../Assets/ProjectImages/PigDice.png';
 import RoboPals from '../../Assets/ProjectImages/RoboPals.png';
@@ -18,7 +18,7 @@ const Projects = () => (
             <Carousel className="h-100">
                 <Carousel.Item className="CarouselItem">
                     <img
-                    className="d-block mx-auto pt-5"
+                    className="d-block mx-auto mt-5 rounded"
                     src={FacialApp}
                     alt="First slide"
                     />
@@ -38,7 +38,7 @@ const Projects = () => (
                 </Carousel.Item>
                 <Carousel.Item className="CarouselItem">
                     <img
-                    className="d-block mx-auto rounded pt-5"
+                    className="d-block mx-auto rounded mt-5"
                     src={PigDice}
                     alt="Third slide"
                     />
@@ -57,7 +57,7 @@ const Projects = () => (
                 </Carousel.Item>
                 <Carousel.Item className="CarouselItem">
                     <img
-                    className="d-block mx-auto rounded pt-5"
+                    className="d-block mx-auto rounded mt-5"
                     src= {RoboPals}
                     alt="Third slide"
                     />
@@ -76,13 +76,32 @@ const Projects = () => (
                 </Carousel.Item>
                 <Carousel.Item className="CarouselItem">
                     <img
-                    className="d-block mx-auto rounded pt-5"
+                    className="d-block mx-auto rounded mt-5"
                     src= {TheBills}
                     alt="Third slide"
                     />
                         <Carousel.Caption className='CarouselText'>
                             <h3>The Bills Band Page</h3>
                             <p>Band page built in React</p>
+                            <div>
+                                <a target="blank" href="https://github.com/lukesaylor">
+                                    <Githublogo className="ProjectGitIcon"/>
+                                </a>
+                                <a target="blank" href="https://github.com/lukesaylor">
+                                    <img src={LiveIcon} alt="live" className="LiveIcon"/>
+                                </a>
+                            </div>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item className="CarouselItem">
+                    <img 
+                    className="d-block mx-auto rounded mt-5"
+                    src= {DevPortfolio}
+                    alt="Third slide"
+                    />
+                        <Carousel.Caption className='CarouselText'>
+                            <h3>Developer Portfolio</h3>
+                            <p>Portfolio site built with React, Bootstrap, and NodeJs</p>
                             <div>
                                 <a target="blank" href="https://github.com/lukesaylor">
                                     <Githublogo className="ProjectGitIcon"/>
