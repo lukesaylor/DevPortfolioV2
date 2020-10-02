6d7fe69cb12e4f03a79cc41f21e79f92a437cc32
Lukas Saylor <slappyhoursound@gmail.com>
2020-10-02 15:40:08 -0400

    social media links

--- src/components/SocialMediaLinks/SocialMediaLinks.js
+++ src/components/SocialMediaLinks/SocialMediaLinks.js
@@ -1,16 +1,12 @@
 import React from "react";
 import "./SocialMediaLinks.css";
 import { ReactComponent as Githublogo } from "../../Assets/github.svg";
-import { ReactComponent as Leetcodelogo } from "../../Assets/leetcode.svg";
 import { ReactComponent as Linkedinlogo } from "../../Assets/linkedin.svg";
 
 const SocialMediaLinks = () => (
   <div className="SocialMediaContainer">
     <a target="blank" href="https://github.com/lukesaylor">
       <Githublogo className="SocialIcon" />
-    </a>
-    <a target="blank" href="https://leetcode.com/bigpancake/">
-      <Leetcodelogo className="SocialIcon" />
     </a>
     <a target="blank" href="http://www.linkedin.com/in/LukasSaylor/">
       <Linkedinlogo className="SocialIcon" />
