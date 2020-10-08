cdb4a9efe3ddef9a9eea7fe9002cb5f5588585fe
Stephanie Altman <slappyhoursound@gmail.com>
2020-10-08 13:32:26 -0400

    old files removed

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -9,7 +9,7 @@ const Projects = () => {
       <h1 className="Projectsheader">Projects</h1>
       <div className="ProjectsContent">
         <ProjectCard 
-          title="Band Page"
+          title="Musician Page"
           image="TheBills"
           text="Band page built in React with custom graphical elements designed in procreate"
           codelink="https://github.com/lukesaylor/billsbandpage"
@@ -25,11 +25,18 @@ const Projects = () => {
           livelink="https://www.google.com"
         />
         <ProjectCard 
-          title="WhatsApp clone"
+          title="WhatsApp Clone"
           image="whatsapp"
           text="Likeness of the Whatsapp web messaging app built in React. Utilizes a Firebase realtime database for message storage. Google Sign-in for authentication and user data. Hosted on Firebase"
           codelink="https://github.com/lukesaylor/whatsappclone"
           livelink="https://wutsappclone.web.app/"
+        />
+        <ProjectCard 
+          title="COVID-19 Tracking App"
+          image="covidapp"
+          text="Coronavirus data resource built with React. Data is provided by Covid Data API and displayed with ChartJS. UI elements created with Material UI"
+          codelink="https://github.com/lukesaylor/covidtracker"
+          livelink="https://lukesaylor.github.io/covidtracker/#/"
         />
       </div>
     </div>
