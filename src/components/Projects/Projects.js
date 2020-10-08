9ea289251e561f42284612968c0a83410cdebb90
Stephanie Altman <slappyhoursound@gmail.com>
2020-10-08 14:22:11 -0400

    new project added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,37 +8,44 @@ const Projects = () => {
       <h1 className="Projectsheader">Projects</h1>
       <div className="ProjectsContent">
         <ProjectCard
+          title="React Quiz App"
+          image="quizapp"
+          text="Quiz application built with Typescript React. Trivia questions provided by opentdb."
+          codelink="https://github.com/lukesaylor/reactquiz"
+          livelink="https://lukesaylor.github.io/reactquiz/"
+        />
+        <ProjectCard
           title="WhatsApp Clone"
           image="whatsapp"
-          text="Likeness of the Whatsapp web messaging app built in React. Utilizes a Firebase realtime database for message storage. Google Sign-in for authentication and user data. Hosted on Firebase"
+          text="Likeness of the Whatsapp web messaging app built in React. Utilizes a Firebase realtime database for message storage. Google Sign-in for authentication and user data. Hosted on Firebase."
           codelink="https://github.com/lukesaylor/whatsappclone"
           livelink="https://wutsappclone.web.app/"
         />
         <ProjectCard
           title="COVID-19 Tracking App"
           image="covidapp"
-          text="Coronavirus data resource built with React. Data is provided by Covid Data API and displayed with ChartJS. UI elements created with Material UI"
+          text="Coronavirus data resource built with React. Data is provided by Covid Data API and displayed with ChartJS. UI elements created with Material UI."
           codelink="https://github.com/lukesaylor/covidtracker"
           livelink="https://lukesaylor.github.io/covidtracker/#/"
         />
         <ProjectCard
           title="Web Chat Application"
           image="chatapp"
-          text="Realtime React chat application utilizing socket.io for websocket connections in user creatable chatrooms"
+          text="Realtime React chat application utilizing socket.io for websocket connections in user creatable chatrooms."
           codelink="https://github.com/lukesaylor/covidtracker"
           livelink="https://lukesaylor.github.io/covidtracker/#/"
         />
         <ProjectCard
           title="Musician Page"
           image="TheBills"
-          text="Band page built in React with custom graphical elements designed in procreate"
+          text="Band page built in React with custom graphical elements designed in procreate."
           codelink="https://github.com/lukesaylor/billsbandpage"
           livelink="https://lukesaylor.github.io/billsbandpage/#/"
         />
         <ProjectCard
           title="Facial Recognition App"
           image="FacialApp"
-          text="This is a React App that identifies faces within images using the image URL and Clarafai facial recognition API"
+          text="This is a React App that identifies faces within images using the image URL and Clarafai facial recognition API."
           codelink="https://github.com/lukesaylor/facialrecogapp"
           livelink="https://www.google.com"
         />
